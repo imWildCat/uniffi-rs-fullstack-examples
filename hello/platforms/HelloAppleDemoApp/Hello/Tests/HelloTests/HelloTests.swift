a0911cbd0f9cf3854c5128a71cd114187d5c49bb
Daohan Chong <dachon@microsoft.com>
2022-01-24 14:21:04 +0800

    Add semicolon of UDL back and improve tests

--- hello/platforms/HelloAppleDemoApp/Hello/Tests/HelloTests/HelloTests.swift
+++ hello/platforms/HelloAppleDemoApp/Hello/Tests/HelloTests/HelloTests.swift
@@ -5,8 +5,6 @@ final class HelloTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//      XCTAssertEqual(Hello.text, "Hello, World!")
       XCTAssertEqual(rustGreeting(name: "Bob"), "Hello, Bob!")
     }
 }
