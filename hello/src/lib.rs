143bc63ebcad76e074ee1c460c59441dfd1d37de
imWildCat <wildcat.name@gmail.com>
2023-08-20 13:03:33 -0700

    trying to fix

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,5 +1,11 @@
+uniffi::setup_scaffolding!();
+// This must be put at the top
+
+
 pub fn rust_greeting(to: String) -> String {
     return format!("Hello, {}!", to);
 }
 
-include!(concat!(env!("OUT_DIR"), "/hello.uniffi.rs"));
+// uniffi::include_scaffolding!("hello");
+// We don't need this because of uniffi::setup_scaffolding()
+// Please refer to <https://mozilla.github.io/uniffi-rs/proc_macro/index.html>
