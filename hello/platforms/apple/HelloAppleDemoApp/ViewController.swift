131bc92052f6d019ee9582f3e7373f4b8768a9d3
Daohan Chong <wildcat.name@gmail.com>
2023-09-20 21:35:44 -0700

    Use new macros system of Uniffi (#11)
    * add workspace
    
    * trying to fix
    
    * improve gen script
    
    * try macos-latest-xl
    
    * Revert "try macos-latest-xl"
    
    This reverts commit 19f607489c63aa255a1cd952d5b01a63124bb624.
    
    * checkout v3
    
    * trying
    
    * use latest main of uniffi
    
    * fix bidgen swift
    
    * Revert "fix bidgen swift"
    
    This reverts commit 5bd84189b1182a2db3548ec59714f1b2b90170c8.
    
    * update
    
    * update add async
    
    * fix
    
    * fix
    
    * trying to fix android
    
    * ndkPath

--- hello/platforms/apple/HelloAppleDemoApp/ViewController.swift
+++ hello/platforms/apple/HelloAppleDemoApp/ViewController.swift
@@ -7,8 +7,14 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    print(rustGreeting(name: "Bob"))
+//     Do any additional setup after loading the view.
+    print(rustGreeting(to: "Bob"))
+    print(Hello.add(a: 1, b: 2))
+    
+    Task {
+      let str = await Hello.sayAfter(ms: 1500, who: "Async")
+      print(str)
+    }
   }
 }
 
--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,12 +1,59 @@
-// uniffi::setup_scaffolding!();
 uniffi::include_scaffolding!("hello");
-// This must be put at the top
+// This is interesting. Because we're supposed to use setup_scaffolding!() at the top.
+// Please refer to <https://mozilla.github.io/uniffi-rs/proc_macro/index.html>
+// I found this sample at: https://github.com/MathieuTricoire/convex-rs-ffi/tree/90fb36ea3dec16b05a8e4f47aa032987b2727122
+// uniffi::setup_scaffolding!();
+use async_std::future::{pending, timeout};
 
+#[uniffi::export(callback_interface)]
+pub trait GreetingDelegate: Send + Sync {
+    fn greeting_called(&self, to: String);
+}
 
+pub struct GreetingLogger {
+    delegate: Box<dyn GreetingDelegate>,
+}
+
+impl GreetingLogger {
+    pub fn new(delegate: Box<dyn GreetingDelegate>) -> Self {
+        Self { delegate }
+    }
+
+    pub fn greeting_called(&self, to: String) {
+        self.delegate.greeting_called(to)
+    }
+}
+
+static LOGGER_INSTANCE: once_cell::sync::OnceCell<GreetingLogger> =
+    once_cell::sync::OnceCell::new();
+
+#[uniffi::export]
+pub fn set_logging_delegate(delegate: Box<dyn GreetingDelegate>) {
+    let logger = GreetingLogger::new(delegate);
+    let result = LOGGER_INSTANCE.set(logger);
+    if result.is_err() {
+        panic!("Logger already set");
+    }
+}
+
+#[uniffi::export]
 pub fn rust_greeting(to: String) -> String {
+    if let Some(logger) = LOGGER_INSTANCE.get() {
+        logger.greeting_called(to.clone());
+    }
     return format!("Hello, {}!", to);
 }
 
-// uniffi::include_scaffolding!("hello");
-// We don't need this because of uniffi::setup_scaffolding()
-// Please refer to <https://mozilla.github.io/uniffi-rs/proc_macro/index.html>
+#[uniffi::export]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[uniffi::export]
+pub async fn say_after(ms: u64, who: String) -> String {
+    let never = pending::<()>();
+    timeout(std::time::Duration::from_millis(ms), never)
+        .await
+        .unwrap_err();
+    format!("Hello, {who}!")
+}
